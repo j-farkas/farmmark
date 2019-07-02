03428853329dac2c8d7ebfc46c4b1d2e6f1d3b70
aj ancheta and brian hensley <aj+brian@>
2019-07-02 15:37:58 -0700

    this bad boy will list all that stuff in a paragraph

--- src/components/Produce.jsx
+++ src/components/Produce.jsx
@@ -10,12 +10,14 @@ function Produce(props) {
                 }
             `}</style>
             <h2>{props.month}</h2>
-            <h3>Available produce: {props.selection}</h3>
-            <hr/>
+            <p>
+                {props.selection.map((item, index) =>
+                    <span>{item}, </span>
+                )}
+            </p>
         </div>
     );
 }
-
 Produce.propTypes = {
     month: PropTypes.string.isRequired,
     selection: PropTypes.array.isRequired
